28de4741abe8f3da58ea460427ebef919d8afa8c
DischordDynne <wgibbs08@outlook.com>
2025-08-16 00:42:42 -0400

    create repository

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add(a: u64, b: u64) -> u64 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
