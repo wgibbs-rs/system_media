1a0f3a9d23545a3df6608ba6686bed29eb3db9d5
DischordDynne <wgibbs08@outlook.com>
2025-08-18 00:29:15 -0400

    fix formatting

--- build.rs
+++ build.rs
@@ -4,8 +4,6 @@ use std::process::Command;
 #[cfg(target_os = "macos")]
 fn main() {
     let swift_file = "src/macos/nowplaying.swift";
-
-    // Output directory for compiled library
     let out_dir = env::var("OUT_DIR").unwrap();
     let lib_path = format!("{}/libswiftlib.a", out_dir);
 
